f2843b01d7b67210a2e5693bd7d743a47d76ea39
Drofseh <drofseh@gmail.com>
2017-07-19 13:53:10 -0700

    UK Units Initial Commit
    - Adds UK defintions and faction

---
+++ Addons/2PzD_Custom_Units/definitions/British Definitions.hpp
@@ -0,0 +1,20 @@
+
+//Equipment
+#define UK_Def_EqI      
+#define UK_Def_EqL      "ItemCompass","ItemWatch"
+#define UK_Ldr_EqL      
+#define UK_Med          
+
+//Magazines
+#define UK_Mags_LeeN    "fow_10Rnd_303","fow_10Rnd_303","fow_10Rnd_303","fow_10Rnd_303","fow_10Rnd_303","fow_10Rnd_303"
+#define UK_Mags_Thmp20  "LIB_20Rnd_45ACP_M1_2PzD","LIB_20Rnd_45ACP_M1_2PzD","LIB_20Rnd_45ACP_M1_2PzD"
+#define UK_Mags_Thmp30  "LIB_30Rnd_45ACP","LIB_30Rnd_45ACP","LIB_30Rnd_45ACP"
+#define UK_Mags_Sten    "LEN_32Rnd_9x19","LEN_32Rnd_9x19","LEN_32Rnd_9x19"
+#define UK_Mags_Bren    "fow_30Rnd_303_bren","fow_30Rnd_303_bren","fow_30Rnd_303_bren","fow_30Rnd_303_bren"
+#define UK_Mags_Web455  "fow_6Rnd_455","fow_6Rnd_455","fow_6Rnd_455","fow_6Rnd_455",
+#define UK_Mags_HiPwr   "LEN_13Rnd_9x19","LEN_13Rnd_9x19","LEN_13Rnd_9x19","LEN_13Rnd_9x19"
+#define UK_Mags_Welrd   "LEN_8Rnd_765x17_2PzD","LEN_8Rnd_765x17_2PzD","LEN_8Rnd_765x17_2PzD","LEN_8Rnd_765x17_2PzD"
+
+#define UK_Mags_Boys    "LIB_1Rnd_145x114","LIB_1Rnd_145x114","LIB_1Rnd_145x114","LIB_1Rnd_145x114","LIB_1Rnd_145x114","LIB_1Rnd_145x114","LIB_1Rnd_145x114","LIB_1Rnd_145x114","LIB_1Rnd_145x114","LIB_1Rnd_145x114","LIB_1Rnd_145x114","LIB_1Rnd_145x114","LIB_1Rnd_145x114","LIB_1Rnd_145x114","LIB_1Rnd_145x114","LIB_1Rnd_145x114","LIB_1Rnd_145x114","LIB_1Rnd_145x114","LIB_1Rnd_145x114","LIB_1Rnd_145x114"
+#define UK_Mags_PiatAT  "fow_1Rnd_piat","fow_1Rnd_piat","fow_1Rnd_piat"
+#define UK_Mags_MHE     "LIB_1Rnd_60mm_Mo_HE","LIB_1Rnd_60mm_Mo_HE","LIB_1Rnd_60mm_Mo_HE"
---
+++ Addons/2PzD_Custom_Units/factions/british.hpp
@@ -0,0 +1,9 @@
+
+    class 2PzD_UK_Army_39 {
+
+        displayName = "!2PzD British Army 1939";
+        icon = "";
+        priority = 1;
+        side = 1;
+
+    };
