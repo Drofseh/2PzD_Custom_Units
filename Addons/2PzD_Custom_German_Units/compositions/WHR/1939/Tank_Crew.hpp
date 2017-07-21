ec0ee0fd22d53ab913efe6e27b5dddc03c00a986
Drofseh <Drofseh@users.noreply.github.com>
2017-07-21 01:31:14 -0700

    G WHR 1939 groups (#5)
    - Add German Wehrmacht groups for 1939
    - Add Licence
    - Some small formatting changes

---
+++ Addons/2PzD_Custom_German_Units/compositions/WHR/1939/Tank_Crew.hpp
@@ -0,0 +1,12 @@
+    class 2PzD_WHR_1939_Tank_Crew {
+
+        faction = "2PzD_Ger_WHR_39";
+        name = "Tank Crew";
+        side = "1";
+
+        class Object1 {side=8;vehicle="2PzD_G_WHR_39_VCom";rank="";position[]={0,1,0};dir=0;};
+        class Object2 {side=8;vehicle="2PzD_G_WHR_39_VCrew";rank="";position[]={0,-1,0};dir=0;};
+        class Object3 {side=8;vehicle="2PzD_G_WHR_39_VCrew";rank="";position[]={-1,-1,0};dir=0;};
+        class Object4 {side=8;vehicle="2PzD_G_WHR_39_VCrew";rank="";position[]={1,-1,0};dir=0;};
+
+    }; // End 2PzD_WHR_1939_Tank_Crew
