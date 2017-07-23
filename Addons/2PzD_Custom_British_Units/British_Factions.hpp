88202f3b0b8b6bb4c54c21c1f6426cf6fda1ab04
Drofseh <Drofseh@users.noreply.github.com>
2017-07-22 19:56:46 -0700

    Dev 0.1.0 (#8)
    - Add Licence
    - Add version number to Readme
    - Add 1939 German infantry units and compositions
    - Add UK Units from 1939 to 1945
    - Add Contributing Guide to the Wiki, Fix #4 
    - Change some file structure layouts
    - Update contributing.md

---
+++ Addons/2PzD_Custom_British_Units/British_Factions.hpp
@@ -0,0 +1,45 @@
+
+    class 2PzD_UK_Army_39 {
+
+        displayName = "!2PzD British Army 1939/40";
+        icon = "";
+        priority = 1;
+        side = 1;
+
+    };
+
+    class 2PzD_UK_Army_41 {
+
+        displayName = "!2PzD British Army 1941";
+        icon = "";
+        priority = 1;
+        side = 1;
+
+    };
+
+    class 2PzD_UK_Army_42 {
+
+        displayName = "!2PzD British Army 1942";
+        icon = "";
+        priority = 1;
+        side = 1;
+
+    };
+
+    class 2PzD_UK_Army_44 {
+
+        displayName = "!2PzD British Army 1944";
+        icon = "";
+        priority = 1;
+        side = 1;
+
+    };
+
+    class 2PzD_UK_Army_45 {
+
+        displayName = "!2PzD British Army 1945";
+        icon = "";
+        priority = 1;
+        side = 1;
+
+    };
---
+++ Addons/2PzD_Custom_British_Units/units/UK_Army_1943.hpp
@@ -0,0 +1,350 @@
+
+//Company HQ
+
+    class 2PzD_UK_Army_43_CC : 2PzD_UK_Army_41_CC {
+        editorPreview = "\2PzD_Custom_British_Units\images\editor previews\2PzD_UK_Army_43_CC.jpg"
+        faction = "2PzD_UK_Army_42";
+
+        //Items
+        linkedItems[] = {"fow_h_uk_mk2_net","fow_v_uk_officer",UK_Def_EqL,UK_Ldr_EqL};
+        respawnLinkedItems[] = {"fow_h_uk_mk2_net","fow_v_uk_officer",UK_Def_EqL,UK_Ldr_EqL};
+    };
+
+    class 2PzD_UK_Army_43_C2 : 2PzD_UK_Army_41_C2 {
+        editorPreview = "\2PzD_Custom_British_Units\images\editor previews\2PzD_UK_Army_43_C2.jpg"
+        faction = "2PzD_UK_Army_42";
+
+        //Items
+        linkedItems[] = {"fow_h_uk_mk2_net","fow_v_uk_sten",UK_Def_EqL,UK_Ldr_EqL};
+        respawnLinkedItems[] = {"fow_h_uk_mk2_net","fow_v_uk_sten",UK_Def_EqL,UK_Ldr_EqL};
+
+        magazines[] = {UK_Mags_Sten,UK_Mags_Sten,"SmokeShell","SmokeShell","HandGrenade"};
+        respawnMagazines[] = {UK_Mags_Sten,UK_Mags_Sten,"SmokeShell","SmokeShell","HandGrenade"};
+
+        weapons[] = {"LEN_StenMk2","Throw","Put"};
+        respawnWeapons[] = {"LEN_StenMk2","Throw","Put"};
+    };
+
+//Platoon HQ
+
+    class 2PzD_UK_Army_43_PC : 2PzD_UK_Army_41_PC {
+        editorPreview = "\2PzD_Custom_British_Units\images\editor previews\2PzD_UK_Army_43_PC.jpg"
+        faction = "2PzD_UK_Army_42";
+
+        //Items
+        linkedItems[] = {"fow_h_uk_mk2_net","fow_v_uk_officer",UK_Def_EqL,UK_Ldr_EqL};
+        respawnLinkedItems[] = {"fow_h_uk_mk2_net","fow_v_uk_officer",UK_Def_EqL,UK_Ldr_EqL};
+    };
+
+    class 2PzD_UK_Army_43_P2 : 2PzD_UK_Army_41_P2 {
+        editorPreview = "\2PzD_Custom_British_Units\images\editor previews\2PzD_UK_Army_43_P2.jpg"
+        faction = "2PzD_UK_Army_42";
+
+        //Items
+        linkedItems[] = {"fow_h_uk_mk2_net","fow_v_uk_sten",UK_Def_EqL,UK_Ldr_EqL};
+        respawnLinkedItems[] = {"fow_h_uk_mk2_net","fow_v_uk_sten",UK_Def_EqL,UK_Ldr_EqL};
+
+        magazines[] = {UK_Mags_Sten,UK_Mags_Sten,"SmokeShell","SmokeShell","HandGrenade"};
+        respawnMagazines[] = {UK_Mags_Sten,UK_Mags_Sten,"SmokeShell","SmokeShell","HandGrenade"};
+
+        weapons[] = {"LEN_StenMk2","Throw","Put"};
+        respawnWeapons[] = {"LEN_StenMk2","Throw","Put"};
+    };
+
+    class 2PzD_UK_Army_43_Med : 2PzD_UK_Army_41_Med {
+        editorPreview = "\2PzD_Custom_British_Units\images\editor previews\2PzD_UK_Army_43_Med.jpg"
+        faction = "2PzD_UK_Army_42";
+
+        //Items
+        linkedItems[] = {"fow_h_uk_mk2_net","fow_v_uk_base",UK_Def_EqL};
+        respawnLinkedItems[] = {"fow_h_uk_mk2_net","fow_v_uk_base",UK_Def_EqL};
+    };
+
+    class 2PzD_UK_Army_43_Sig : 2PzD_UK_Army_41_Sig {
+        editorPreview = "\2PzD_Custom_British_Units\images\editor previews\2PzD_UK_Army_43_Sig.jpg"
+        faction = "2PzD_UK_Army_42";
+
+        //Items
+        linkedItems[] = {"fow_h_uk_mk2_net","fow_v_uk_base",UK_Def_EqL};
+        respawnLinkedItems[] = {"fow_h_uk_mk2_net","fow_v_uk_base",UK_Def_EqL};
+    };
+
+    class 2PzD_UK_Army_43_LMortG : 2PzD_UK_Army_41_LMortG {
+        editorPreview = "\2PzD_Custom_British_Units\images\editor previews\2PzD_UK_Army_43_LMortG.jpg"
+        faction = "2PzD_UK_Army_42";
+
+        //Items
+        linkedItems[] = {"fow_h_uk_mk2_net","fow_v_uk_officer","ACE_MapTools","ACE_RangeTable_82mm"};
+        respawnLinkedItems[] = {"fow_h_uk_mk2_net","fow_v_uk_officer","ACE_MapTools","ACE_RangeTable_82mm"};
+    };
+
+    class 2PzD_UK_Army_43_LMortA : 2PzD_UK_Army_41_LMortA {
+        editorPreview = "\2PzD_Custom_British_Units\images\editor previews\2PzD_UK_Army_43_LMortA.jpg"
+        faction = "2PzD_UK_Army_42";
+
+        //Items
+        linkedItems[] = {"fow_h_uk_mk2_net","fow_v_uk_officer","ACE_MapTools","ACE_RangeTable_82mm"};
+        respawnLinkedItems[] = {"fow_h_uk_mk2_net","fow_v_uk_officer","ACE_MapTools","ACE_RangeTable_82mm"};
+    };
+
+//Squad
+
+    class 2PzD_UK_Army_43_SL : 2PzD_UK_Army_41_SL {
+        editorPreview = "\2PzD_Custom_British_Units\images\editor previews\2PzD_UK_Army_43_SL.jpg"
+        faction = "2PzD_UK_Army_42";
+
+        //Items
+        linkedItems[] = {"fow_h_uk_mk2_net","fow_v_uk_sten",UK_Def_EqL,UK_Ldr_EqL};
+        respawnLinkedItems[] = {"fow_h_uk_mk2_net","fow_v_uk_sten",UK_Def_EqL,UK_Ldr_EqL};
+
+        magazines[] = {UK_Mags_Sten,UK_Mags_Sten,"SmokeShell","SmokeShell","HandGrenade","fow_30Rnd_303_bren","fow_30Rnd_303_bren"};
+        respawnMagazines[] = {UK_Mags_Sten,UK_Mags_Sten,"SmokeShell","SmokeShell","HandGrenade","fow_30Rnd_303_bren","fow_30Rnd_303_bren"};
+
+        weapons[] = {"LEN_StenMk2","Throw","Put"};
+        respawnWeapons[] = {"LEN_StenMk2","Throw","Put"};
+    };
+
+    class 2PzD_UK_Army_43_TL : 2PzD_UK_Army_41_TL {
+        editorPreview = "\2PzD_Custom_British_Units\images\editor previews\2PzD_UK_Army_43_TL.jpg"
+        faction = "2PzD_UK_Army_42";
+
+        //Items
+        linkedItems[] = {"fow_h_uk_mk2_net","fow_v_uk_sten",UK_Def_EqL,UK_Ldr_EqL};
+        respawnLinkedItems[] = {"fow_h_uk_mk2_net","fow_v_uk_sten",UK_Def_EqL,UK_Ldr_EqL};
+
+        magazines[] = {UK_Mags_Sten,UK_Mags_Sten,"SmokeShell","SmokeShell","HandGrenade","fow_30Rnd_303_bren","fow_30Rnd_303_bren"};
+        respawnMagazines[] = {UK_Mags_Sten,UK_Mags_Sten,"SmokeShell","SmokeShell","HandGrenade","fow_30Rnd_303_bren","fow_30Rnd_303_bren"};
+
+        weapons[] = {"LEN_StenMk2","Throw","Put"};
+        respawnWeapons[] = {"LEN_StenMk2","Throw","Put"};
+    };
+
+    class 2PzD_UK_Army_43_Rif : 2PzD_UK_Army_41_Rif {
+        editorPreview = "\2PzD_Custom_British_Units\images\editor previews\2PzD_UK_Army_43_Rif.jpg"
+        faction = "2PzD_UK_Army_42";
+
+        //Items
+        linkedItems[] = {"fow_h_uk_mk2_net","fow_v_uk_base",UK_Def_EqL};
+        respawnLinkedItems[] = {"fow_h_uk_mk2_net","fow_v_uk_base",UK_Def_EqL};
+    };
+
+    class 2PzD_UK_Army_43_AR : 2PzD_UK_Army_41_AR {
+        editorPreview = "\2PzD_Custom_British_Units\images\editor previews\2PzD_UK_Army_43_AR.jpg"
+        faction = "2PzD_UK_Army_42";
+
+        //Items
+        linkedItems[] = {"fow_h_uk_mk2_net","fow_v_uk_bren",UK_Def_EqL};
+        respawnLinkedItems[] = {"fow_h_uk_mk2_net","fow_v_uk_bren",UK_Def_EqL};
+    };
+
+    class 2PzD_UK_Army_43_AAR : 2PzD_UK_Army_41_AAR {
+        editorPreview = "\2PzD_Custom_British_Units\images\editor previews\2PzD_UK_Army_43_AAR.jpg"
+        faction = "2PzD_UK_Army_42";
+
+        //Items
+        linkedItems[] = {"fow_h_uk_mk2_net","fow_v_uk_bren","LIB_Binocular_SU",UK_Def_EqL};
+        respawnLinkedItems[] = {"fow_h_uk_mk2_net","fow_v_uk_bren","LIB_Binocular_SU",UK_Def_EqL};
+    };
+
+    class 2PzD_UK_Army_43_ARAB : 2PzD_UK_Army_41_ARAB {
+        editorPreview = "\2PzD_Custom_British_Units\images\editor previews\2PzD_UK_Army_43_ARAB.jpg"
+        faction = "2PzD_UK_Army_42";
+
+        //Items
+        linkedItems[] = {"fow_h_uk_mk2_net","fow_v_uk_bren",UK_Def_EqL};
+        respawnLinkedItems[] = {"fow_h_uk_mk2_net","fow_v_uk_bren",UK_Def_EqL};
+    };
+
+//Machine Gun Team
+
+    class 2PzD_UK_Army_43_MGTL : 2PzD_UK_Army_41_MGTL {
+        editorPreview = "\2PzD_Custom_British_Units\images\editor previews\2PzD_UK_Army_43_MGTL.jpg"
+        faction = "2PzD_UK_Army_42";
+
+        //Items
+        linkedItems[] = {"fow_h_uk_mk2_net","fow_v_uk_base",UK_Def_EqL,UK_Ldr_EqL};
+        respawnLinkedItems[] = {"fow_h_uk_mk2_net","fow_v_uk_base",UK_Def_EqL,UK_Ldr_EqL};
+    };
+
+    class 2PzD_UK_Army_43_MG : 2PzD_UK_Army_41_MG {
+        editorPreview = "\2PzD_Custom_British_Units\images\editor previews\2PzD_UK_Army_43_MG.jpg"
+        faction = "2PzD_UK_Army_42";
+
+        //Items
+        linkedItems[] = {"fow_h_uk_mk2_net","fow_v_uk_officer",UK_Def_EqL};
+        respawnLinkedItems[] = {"fow_h_uk_mk2_net","fow_v_uk_officer",UK_Def_EqL};
+    };
+
+    class 2PzD_UK_Army_43_MGA : 2PzD_UK_Army_41_MGA {
+        editorPreview = "\2PzD_Custom_British_Units\images\editor previews\2PzD_UK_Army_43_MGA.jpg"
+        faction = "2PzD_UK_Army_42";
+
+        //Items
+        linkedItems[] = {"fow_h_uk_mk2_net","fow_v_uk_officer",UK_Def_EqL,UK_Ldr_EqL};
+        respawnLinkedItems[] = {"fow_h_uk_mk2_net","fow_v_uk_officer",UK_Def_EqL,UK_Ldr_EqL};
+    };
+
+    class 2PzD_UK_Army_43_MGAB : 2PzD_UK_Army_41_MGAB {
+        editorPreview = "\2PzD_Custom_British_Units\images\editor previews\2PzD_UK_Army_43_MGAB.jpg"
+        faction = "2PzD_UK_Army_42";
+
+        //Items
+        linkedItems[] = {"fow_h_uk_mk2_net","fow_v_uk_base",UK_Def_EqL};
+        respawnLinkedItems[] = {"fow_h_uk_mk2_net","fow_v_uk_base",UK_Def_EqL};
+    };
+
+//Mortar Team
+
+    class 2PzD_UK_Army_43_MortTL : 2PzD_UK_Army_41_MortTL {
+        editorPreview = "\2PzD_Custom_British_Units\images\editor previews\2PzD_UK_Army_43_MortTL.jpg"
+        faction = "2PzD_UK_Army_42";
+
+        //Items
+        linkedItems[] = {"fow_h_uk_mk2_net","fow_v_uk_base",UK_Def_EqL,UK_Ldr_EqL};
+        respawnLinkedItems[] = {"fow_h_uk_mk2_net","fow_v_uk_base",UK_Def_EqL,UK_Ldr_EqL};
+    };
+
+    class 2PzD_UK_Army_43_MortG : 2PzD_UK_Army_41_MortG {
+        editorPreview = "\2PzD_Custom_British_Units\images\editor previews\2PzD_UK_Army_43_MortG.jpg"
+        faction = "2PzD_UK_Army_42";
+
+        //Items
+        linkedItems[] = {"fow_h_uk_mk2_net","fow_v_uk_officer",UK_Def_EqL,"ACE_MapTools","ACE_RangeTable_82mm"};
+        respawnLinkedItems[] = {"fow_h_uk_mk2_net","fow_v_uk_officer",UK_Def_EqL,"ACE_MapTools","ACE_RangeTable_82mm"};
+    };
+
+    class 2PzD_UK_Army_43_MortA : 2PzD_UK_Army_41_MortA {
+        editorPreview = "\2PzD_Custom_British_Units\images\editor previews\2PzD_UK_Army_43_MortA.jpg"
+        faction = "2PzD_UK_Army_42";
+
+        //Items
+        linkedItems[] = {"fow_h_uk_mk2_net","fow_v_uk_officer",UK_Def_EqL,"ACE_MapTools","ACE_RangeTable_82mm"};
+        respawnLinkedItems[] = {"fow_h_uk_mk2_net","fow_v_uk_officer",UK_Def_EqL,"ACE_MapTools","ACE_RangeTable_82mm"};
+    };
+
+    class 2PzD_UK_Army_43_MortAB : 2PzD_UK_Army_41_MortAB {
+        editorPreview = "\2PzD_Custom_British_Units\images\editor previews\2PzD_UK_Army_43_MortAB.jpg"
+        faction = "2PzD_UK_Army_42";
+
+        //Items
+        linkedItems[] = {"fow_h_uk_mk2_net","fow_v_uk_base",UK_Def_EqL};
+        respawnLinkedItems[] = {"fow_h_uk_mk2_net","fow_v_uk_base",UK_Def_EqL};
+    };
+
+//Boys AT Rifle Team
+
+    class 2PzD_UK_Army_43_ATRTL : 2PzD_UK_Army_41_ATRTL {
+        editorPreview = "\2PzD_Custom_British_Units\images\editor previews\2PzD_UK_Army_43_ATRTL.jpg"
+        faction = "2PzD_UK_Army_42";
+
+        //Items
+        linkedItems[] = {"fow_h_uk_mk2_net","fow_v_uk_base",UK_Def_EqL,UK_Ldr_EqL};
+        respawnLinkedItems[] = {"fow_h_uk_mk2_net","fow_v_uk_base",UK_Def_EqL,UK_Ldr_EqL};
+    };
+
+    class 2PzD_UK_Army_43_ATRG : 2PzD_UK_Army_41_ATRG {
+        editorPreview = "\2PzD_Custom_British_Units\images\editor previews\2PzD_UK_Army_43_ATRG.jpg"
+        faction = "2PzD_UK_Army_42";
+
+        //Items
+        linkedItems[] = {"fow_h_uk_mk2_net","fow_v_uk_officer",UK_Def_EqL};
+        respawnLinkedItems[] = {"fow_h_uk_mk2_net","fow_v_uk_officer",UK_Def_EqL};
+    };
+
+    class 2PzD_UK_Army_43_ATRAB : 2PzD_UK_Army_41_ATRAB {
+        editorPreview = "\2PzD_Custom_British_Units\images\editor previews\2PzD_UK_Army_43_ATRAB.jpg"
+        faction = "2PzD_UK_Army_42";
+
+        //Items
+        linkedItems[] = {"fow_h_uk_mk2_net","fow_v_uk_base",UK_Def_EqL};
+        respawnLinkedItems[] = {"fow_h_uk_mk2_net","fow_v_uk_base",UK_Def_EqL};
+    };
+
+//PIAT Team
+
+    class 2PzD_UK_Army_43_ATTL : 2PzD_UK_Army_41_ATRTL {
+        editorPreview = "\2PzD_Custom_British_Units\images\editor previews\2PzD_UK_Army_43_ATTL.jpg"
+        faction = "2PzD_UK_Army_42";
+        displayName = "PIAT Team Leader";
+
+        //Items
+        backpack = "fow_b_uk_piat_AT_2PzD";
+
+        linkedItems[] = {"fow_h_uk_mk2_net","fow_v_uk_base",UK_Def_EqL,UK_Ldr_EqL};
+        respawnLinkedItems[] = {"fow_h_uk_mk2_net","fow_v_uk_base",UK_Def_EqL,UK_Ldr_EqL};
+
+        magazines[] = {UK_Mags_LeeEn,"SmokeShell","HandGrenade"};
+        respawnMagazines[] = {UK_Mags_LeeEn,"SmokeShell","HandGrenade"};
+
+        uniformClass = "fow_u_uk_bd40_corporal";
+    };
+
+    class 2PzD_UK_Army_43_ATG : 2PzD_UK_Army_41_ATRG {
+        editorPreview = "\2PzD_Custom_British_Units\images\editor previews\2PzD_UK_Army_43_ATG.jpg"
+        faction = "2PzD_UK_Army_42";
+        displayName = "PIAT Gunner";
+
+        //Items
+        backpack = "fow_b_uk_piat_AT_2PzD";
+
+        Items[] = {UK_Def_EqI};
+        RespawnItems[] = {UK_Def_EqI};
+
+        linkedItems[] = {"fow_h_uk_mk2_net","fow_v_uk_officer",UK_Def_EqL};
+        respawnLinkedItems[] = {"fow_h_uk_mk2_net","fow_v_uk_officer",UK_Def_EqL};
+
+        magazines[] = {UK_Mags_Web455};
+        respawnMagazines[] = {UK_Mags_Web455};
+
+        weapons[] = {"fow_w_webley","LIB_PTRD","Throw","Put"};
+        respawnWeapons[] = {"fow_w_webley","LIB_PTRD","Throw","Put"};
+
+        uniformClass = "fow_u_uk_bd40_private";
+    };
+
+    class 2PzD_UK_Army_43_ATAB : 2PzD_UK_Army_41_ATRAB {
+        editorPreview = "\2PzD_Custom_British_Units\images\editor previews\2PzD_UK_Army_43_ATAB.jpg"
+        faction = "2PzD_UK_Army_42";
+        displayName = "PIAT Ammo Bearer";
+
+        //Items
+        backpack = "fow_b_uk_piat_AT_2PzD";
+
+        linkedItems[] = {"fow_h_uk_mk2_net","fow_v_uk_base",UK_Def_EqL};
+        respawnLinkedItems[] = {"fow_h_uk_mk2_net","fow_v_uk_base",UK_Def_EqL};
+
+        magazines[] = {UK_Mags_LeeEn,"HandGrenade"};
+        respawnMagazines[] = {UK_Mags_LeeEn,"HandGrenade"};
+    };
+
+//Tank Crew
+
+    class 2PzD_UK_Army_43_VCom : 2PzD_UK_Army_41_VCom {
+        editorPreview = "\2PzD_Custom_British_Units\images\editor previews\2PzD_UK_Army_43_VCom.jpg"
+        faction = "2PzD_UK_Army_42";
+
+        //Items
+        linkedItems[] = {"fow_h_uk_mk2_net","fow_v_uk_sten",UK_Def_EqL,UK_Ldr_EqL};
+        respawnLinkedItems[] = {"fow_h_uk_mk2_net","fow_v_uk_sten",UK_Def_EqL,UK_Ldr_EqL};
+
+        magazines[] = {UK_Mags_Sten,UK_Mags_Sten,UK_Mags_Web455,"SmokeShell","SmokeShell","HandGrenade"};
+        respawnMagazines[] = {UK_Mags_Sten,UK_Mags_Sten,UK_Mags_Web455,"SmokeShell","SmokeShell","HandGrenade"};
+
+        weapons[] = {"LEN_StenMk2","fow_w_webley","Throw","Put"};
+        respawnWeapons[] = {"LEN_StenMk2","fow_w_webley","Throw","Put"};
+    };
+
+    class 2PzD_UK_Army_43_VCrew : 2PzD_UK_Army_41_VCrew {
+        editorPreview = "\2PzD_Custom_British_Units\images\editor previews\2PzD_UK_Army_43_VCrew.jpg"
+        faction = "2PzD_UK_Army_42";
+
+        //Items
+        linkedItems[] = {"fow_h_uk_mk2_net","fow_v_uk_sten",UK_Def_EqL};
+        respawnLinkedItems[] = {"fow_h_uk_mk2_net","fow_v_uk_sten",UK_Def_EqL};
+
+        magazines[] = {UK_Mags_Sten,UK_Mags_Sten,UK_Mags_Web455};
+        respawnMagazines[] = {UK_Mags_Sten,UK_Mags_Sten,UK_Mags_Web455};
+
+        weapons[] = {"LEN_StenMk2","fow_w_webley","Throw","Put"};
+        respawnWeapons[] = {"LEN_StenMk2","fow_w_webley","Throw","Put"};
+    };
